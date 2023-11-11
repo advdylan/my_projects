c6c95fd23a38ca56434ffd252cac3ed986c438e3
advdylan <immortalizetimes@gmail.com>
2023-11-11 17:33:09 +0100

    forms

--- django/lecture/tasks/views.py
+++ django/lecture/tasks/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django import forms
+
 
 # Create your views here.
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value = 1, max_value=10)
+
 
 def index(request):
     return render(request, "tasks/index.html",{
@@ -11,5 +18,9 @@ def index(request):
 
 
 def add(request):
-    return render(request, "tasks/add.html")
+
+    return render(request, "tasks/add.html",{
+        "form": NewTaskForm()
+
+    })
 
