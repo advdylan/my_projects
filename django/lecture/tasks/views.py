5f16b41e1b3cfb711c6eacc9263006afdece6acc
advdylan <immortalizetimes@gmail.com>
2023-11-11 17:02:05 +0100

    o co tu chodzi

--- django/lecture/tasks/views.py
+++ django/lecture/tasks/views.py
@@ -5,8 +5,11 @@ from django.shortcuts import render
 tasks = ["foo", "bar", "baz"]
 
 def index(request):
-    return render(request, "tesks/index.html",{
+    return render(request, "tasks/index.html",{
         "tasks": tasks
-
     })
+
+
+def add(request):
+    return render(request, "tasks/add.html")
 
