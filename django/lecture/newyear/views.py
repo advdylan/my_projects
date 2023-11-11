6db4278dc56fa9604005938ebcb51a69c2d74de2
advdylan <immortalizetimes@gmail.com>
2023-11-11 09:25:39 +0100

    123

--- django/lecture/newyear/views.py
+++ django/lecture/newyear/views.py
@@ -6,6 +6,6 @@ import datetime
 
 def index(request):
     now = datetime.datetime.now()
-    return render(request, "newyear/index.html"),{
-        "newyear": now.month == 1 and now.day == 1
-    }
+    return render(request, "newyear/index.html",{
+        "newyear": True
+    })
