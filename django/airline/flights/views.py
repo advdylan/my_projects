bc62f1a129671f92ff499cb4f6b86c712d57af8d
advdylan <immortalizetimes@gmail.com>
2023-11-15 14:49:27 +0100

    1111

--- django/airline/flights/views.py
+++ django/airline/flights/views.py
@@ -8,3 +8,11 @@ def index(request):
         "flights": Flight.objects.all()
     })
 
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all()
+    
+    })
+
