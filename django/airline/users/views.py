849a5e0c2abd6bceb5ec625d3499505afe6b1858
advdylan <immortalizetimes@gmail.com>
2023-11-15 20:53:44 +0100

    koniec na dzisiaj

--- django/airline/users/views.py
+++ django/airline/users/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
     
 def login_view(request):
     if request.method == "POST":
@@ -26,6 +27,9 @@ def login_view(request):
     return render(request, "users/login.html")
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged out"
+    })
 
 
